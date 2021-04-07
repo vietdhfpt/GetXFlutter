b24aa5c9d35c27f23e09eca9d6f5a7b561ebb490
DHV <dhv@gmail.com>
2021-04-07 17:46:25 +0700

    - [f] Add scrol bar in history page

--- getx_flutter/lib/pages/history/history_page.dart
+++ getx_flutter/lib/pages/history/history_page.dart
@@ -70,121 +70,123 @@ class HistoryPage extends StatelessWidget {
 
   Expanded _buildHistories(HistoryController controller) {
     return Expanded(
-      child: ListView.builder(
-        itemCount: controller.histories.length,
-        itemBuilder: (context, index) {
-          // Check transaction is pay balance to wallet
-          controller.getHistoryPayBalance(index);
+      child: CupertinoScrollbar(
+        child: ListView.builder(
+          itemCount: controller.histories.length,
+          itemBuilder: (context, index) {
+            // Check transaction is pay balance to wallet
+            controller.getHistoryPayBalance(index);
 
-          final history = controller.histories[index];
-          final amount = history.status == Message.REFUNDED
-              ? history.info.refundAmount
-              : history.requestParams.buyItems == null
-                  ? history.requestParams.amount
-                  : history.info.merchant.price;
-          final quantity = history.requestParams.buyItems == null
-              ? 1
-              : history.requestParams.buyItems[0].quantity;
-          final totalAmount =
-              history.status == Message.REFUNDED ? amount : amount * quantity;
+            final history = controller.histories[index];
+            final amount = history.status == Message.REFUNDED
+                ? history.info.refundAmount
+                : history.requestParams.buyItems == null
+                    ? history.requestParams.amount
+                    : history.info.merchant.price;
+            final quantity = history.requestParams.buyItems == null
+                ? 1
+                : history.requestParams.buyItems[0].quantity;
+            final totalAmount =
+                history.status == Message.REFUNDED ? amount : amount * quantity;
 
-          return Container(
-            margin: EdgeInsets.only(
-              left: kPaddingTopup,
-              right: kPaddingTopup,
-              bottom: kPaddingTopup,
-            ),
-            height: 60,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(kBorderRadius),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: kPaddingTopup * .7,
-                vertical: kPaddingTopup / 2,
+            return Container(
+              margin: EdgeInsets.only(
+                left: kPaddingTopup,
+                right: kPaddingTopup,
+                bottom: kPaddingTopup,
               ),
-              child: Container(
-                height: 50,
-                child: Row(
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(kPaddingTopup / 3),
-                      child: Container(
-                        width: 45,
-                        height: 30,
-                        child: Image.asset(
-                          'assets/icons/viettel.png', //'assets/icons/icon_default.jpg',
-                          fit: BoxFit.fill,
+              height: 60,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(kBorderRadius),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: kPaddingTopup * .7,
+                  vertical: kPaddingTopup / 2,
+                ),
+                child: Container(
+                  height: 50,
+                  child: Row(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(kPaddingTopup / 3),
+                        child: Container(
+                          width: 45,
+                          height: 30,
+                          child: Image.asset(
+                            'assets/icons/viettel.png', //'assets/icons/icon_default.jpg',
+                            fit: BoxFit.fill,
+                          ),
                         ),
                       ),
-                    ),
-                    SizedBox(width: kPaddingTopup / 2),
-                    Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      SizedBox(width: kPaddingTopup / 2),
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Viettel',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                                Text(
+                                  '$totalAmountđ',
+                                  style: TextStyle(
+                                    color:
+                                        controller.getItemColor(history.status),
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  controller.convertTimeStampToStringDate(
+                                      history.createdAt),
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                                Text(
+                                  controller.getItemStatus(history.status),
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(width: kPaddingTopup / 2),
+                      Row(
                         children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                'Viettel',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black,
-                                  fontSize: 14,
-                                ),
-                              ),
-                              Text(
-                                '$totalAmountđ',
-                                style: TextStyle(
-                                  color:
-                                      controller.getItemColor(history.status),
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 14,
-                                ),
-                              ),
-                            ],
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                controller.convertTimeStampToStringDate(
-                                    history.createdAt),
-                                style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 12,
-                                ),
-                              ),
-                              Text(
-                                controller.getItemStatus(history.status),
-                                style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 12,
-                                ),
-                              ),
-                            ],
+                          Icon(
+                            Icons.arrow_forward_ios,
+                            size: 16,
+                            color: appColor,
                           ),
                         ],
-                      ),
-                    ),
-                    SizedBox(width: kPaddingTopup / 2),
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.arrow_forward_ios,
-                          size: 16,
-                          color: appColor,
-                        ),
-                      ],
-                    )
-                  ],
+                      )
+                    ],
+                  ),
                 ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
     );
   }
